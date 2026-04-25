101343a8170c176b5e7608d44af913405cbcf2d8
MarvouliGames <143141889+MarvouliGames@users.noreply.github.com>
2026-04-25 18:26:39 +0000

    dsfdfsfef

--- public/uv.sw.js
+++ public/uv.sw.js
@@ -1,8 +1,8 @@
 importScripts(
-  "/uv/uv.bundle.js",
-  "/uv/uv.config.js",
-  "/uv/uv.handler.js",
-  "/uv/uv.rewrite.js"
+  "/uv/bundle.js",
+  "/uv/config.js",
+  "/uv/handler.js",
+  "/uv/rewrite.js"
 );
 
 self.addEventListener("fetch", (event) => {
